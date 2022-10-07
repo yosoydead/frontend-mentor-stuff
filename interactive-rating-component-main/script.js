950d72e2af0dac313695bf90e46f392ec43a7826
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2022-10-07 11:55:05 +0300

    rearanjat js

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,18 +1,26 @@
+// constants
+const DATA_RATING_ATTR = "data-rating";
+const CARD_FLIP_CSS_CLASS = "flipped";
+const RATING_SELECTED_CSS_CLASS = 'selected';
+const MAX_RATING_VALUE = 5;
+
+// HTML nodes
 const reviewWrapper = document.querySelector("#Review");
 const ratingUl = document.querySelector("#Review-Modal--body-rating ul");
 const ratingSelects = document.querySelectorAll("#Review-Modal--body-rating .rating-select");
 const submit = document.querySelector("#Review-Modal button");
-
 const resultWrapper = document.querySelector("#Review-Result--content div p");
-const DATA_RATING_ATTR = "data-rating";
-const CARD_FLIP_CSS_CLASS = "flipped";
 
 let rating = null;
 
 const resetSelectedState = () => {
     for(const r of ratingSelects) {
-        r.classList.remove('selected');
+        r.classList.remove(RATING_SELECTED_CSS_CLASS);
     }
+}
+
+const generateRatingString = (rating, maxRating) => {
+    return `You selected ${rating} out of ${maxRating}`;
 }
 
 submit.addEventListener("click", () => {
@@ -20,7 +28,7 @@ submit.addEventListener("click", () => {
         alert("No rating selected! :)");
         return;
     }
-    resultWrapper.textContent = `You selected ${rating} out of 5`;
+    resultWrapper.textContent = generateRatingString(rating, MAX_RATING_VALUE);
     reviewWrapper.classList.add(CARD_FLIP_CSS_CLASS);
 });
 
@@ -28,7 +36,7 @@ for(const r of ratingSelects) {
     r.addEventListener("click", (e) => {
         console.log(e);
         resetSelectedState();
-        e.target.classList.add('selected');
+        e.target.classList.add(RATING_SELECTED_CSS_CLASS);
         rating =  e.target.getAttribute(DATA_RATING_ATTR);
     });
 }
