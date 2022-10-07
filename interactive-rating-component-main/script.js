f6f24f448f680c4d86f3ae339b7a3b6fdfb86a82
Radu Bogdan-Mircea <radu-bogdan@outlook.com>
2022-10-07 11:48:33 +0300

    flipping card on submit

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,9 +1,11 @@
+const reviewWrapper = document.querySelector("#Review");
 const ratingUl = document.querySelector("#Review-Modal--body-rating ul");
 const ratingSelects = document.querySelectorAll("#Review-Modal--body-rating .rating-select");
 const submit = document.querySelector("#Review-Modal button");
 
 const resultWrapper = document.querySelector("#Review-Result--content div p");
 const DATA_RATING_ATTR = "data-rating";
+const CARD_FLIP_CSS_CLASS = "flipped";
 
 let rating = null;
 
@@ -14,8 +16,12 @@ const resetSelectedState = () => {
 }
 
 submit.addEventListener("click", () => {
-    // console.log("rating", rating);
+    if (!rating) {
+        alert("No rating selected! :)");
+        return;
+    }
     resultWrapper.textContent = `You selected ${rating} out of 5`;
+    reviewWrapper.classList.add(CARD_FLIP_CSS_CLASS);
 });
 
 for(const r of ratingSelects) {
